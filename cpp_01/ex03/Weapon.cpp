8b4ce4cdd99ca27231823e57058c351bfd5a700c
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-13 11:50:18 +0200

    Update Weapon.cpp

--- cpp_01/ex03/Weapon.cpp
+++ cpp_01/ex03/Weapon.cpp
@@ -12,24 +12,43 @@
 
 #include "Weapon.hpp"
 
+/**
+ * @brief Gets the type of the weapon.
+ *
+ * @return Constant reference to the weapon type string.
+ */
 const std::string& Weapon:: getType( void )
 {
 	return (this->type);
 }
+
+/**
+ * @brief Sets the type of the weapon.
+ *
+ * @param type New type to assign to the weapon.
+ */
 void Weapon:: setType(std::string type)
 {
 	this->type = type;
 }
 
-
+/**
+ * @brief Constructs a Weapon with a given type.
+ *
+ * @param type The type of the weapon to initialize.
+ */
 Weapon::Weapon( std::string type )
 {
 	this->type = type;
 	std::cout << "Weapon Constructor" << std::endl;
 }
 
+/**
+ * @brief Destructor for Weapon.
+ *
+ * Prints a message indicating the Weapon object is destroyed.
+ */
 Weapon::~Weapon( void )
 {
 	std::cout << "Weapon Destructor" << std::endl;
 }
-
