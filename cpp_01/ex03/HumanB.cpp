64c42cd2e1cc1d403902e9911f9ead7493c02159
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-13 11:44:54 +0200

    Update HumanB.cpp

--- cpp_01/ex03/HumanB.cpp
+++ cpp_01/ex03/HumanB.cpp
@@ -13,16 +13,25 @@
 #include "HumanB.hpp"
 #include "Weapon.hpp"
 
-/*pongo referencia porque así puedo cambiar en el main lo que
-tiene club y así no se me imprime lo que había antes sino que
-se está imprimiendo lo que he modificado ya que lo que estoy
-modificando es club a través de la referencia que estoy
-recibiendo*/
+/**
+ * @brief Sets the weapon reference for HumanB.
+ *
+ * Uses a pointer to store the address of the weapon passed by reference.
+ * This allows changing the weapon externally to affect the HumanB's weapon.
+ *
+ * @param weapon Reference to a Weapon object to assign to HumanB.
+ */
 void HumanB::setWeapon( Weapon& weapon )
 {
 	this->weapon = &weapon;
 }
 
+/**
+ * @brief Makes HumanB attack using its weapon.
+ *
+ * Prints a message showing the attack with the weapon's type.
+ * If no weapon is set, informs that the HumanB does not have a weapon.
+ */
 void HumanB:: attack( void )
 {
 	if (this->weapon == NULL)
@@ -33,6 +42,13 @@ void HumanB:: attack( void )
 	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 }
 
+/**
+ * @brief Constructs a HumanB with a name and no weapon initially.
+ *
+ * Initializes the weapon pointer to NULL and sets the name.
+ *
+ * @param name Name of the HumanB.
+ */
 HumanB::HumanB( std::string name )
 {
 	this->name = name;
@@ -40,6 +56,11 @@ HumanB::HumanB( std::string name )
 	std::cout << "HumanB Constructor" << std::endl;
 }
 
+/**
+ * @brief Destructor for HumanB.
+ *
+ * Prints a message indicating the HumanB object is being destroyed.
+ */
 HumanB::~HumanB( void )
 {
 	std::cout << "HumanB Destructor" << std::endl;
