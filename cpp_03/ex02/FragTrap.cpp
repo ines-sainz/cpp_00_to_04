c69eed477de2fb80243512e5c508dedba24f932f
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-13 20:04:41 +0200

    Update FragTrap.cpp

--- cpp_03/ex02/FragTrap.cpp
+++ cpp_03/ex02/FragTrap.cpp
@@ -12,11 +12,24 @@
 
 #include "FragTrap.hpp"
 
+/**
+ * @brief Requests a high five from others.
+ * 
+ * Displays a message indicating that the FragTrap wants a high five.
+ */
 void	FragTrap::highFivesGuys( void )
 {
 	std::cout << "FragTrap " << this->name << " ask for a high five" << std::endl;
 }
 
+/**
+ * @brief Copy assignment operator for FragTrap.
+ * 
+ * Copies the attributes from another FragTrap instance.
+ * 
+ * @param before The FragTrap instance to copy from.
+ * @return FragTrap& Reference to the assigned FragTrap instance.
+ */
 FragTrap& FragTrap:: operator=(const FragTrap& before)
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -27,12 +40,24 @@ FragTrap& FragTrap:: operator=(const FragTrap& before)
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor for FragTrap.
+ * 
+ * Initializes a new FragTrap by copying another instance.
+ * 
+ * @param before The FragTrap instance to copy.
+ */
 FragTrap::FragTrap(const FragTrap& before)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor for FragTrap.
+ * 
+ * Initializes a FragTrap with default values.
+ */
 FragTrap::FragTrap( void )
 {
 	std::cout << "Default FragTrap Constructor" << std::endl;
@@ -42,6 +67,13 @@ FragTrap::FragTrap( void )
 	this->AttackDamage = 20;
 }
 
+/**
+ * @brief Parameterized constructor for FragTrap.
+ * 
+ * Initializes a FragTrap with a given name and default stats.
+ * 
+ * @param name The name to assign to the FragTrap.
+ */
 FragTrap::FragTrap( std::string name )
 {
 	this->name = name;
@@ -50,8 +82,13 @@ FragTrap::FragTrap( std::string name )
 	this->EnergyPoints = 50;
 	this->AttackDamage = 20;
 }
+
+/**
+ * @brief Destructor for FragTrap.
+ * 
+ * Displays a message when a FragTrap is destroyed.
+ */
 FragTrap::~FragTrap()
 {
 	std::cout << "Default FragTrap Destructor" << std::endl;
 }
-
