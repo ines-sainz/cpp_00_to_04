8b2330a69c4fc73dd0b96cd750e47c02be3b0de8
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-13 11:43:18 +0200

    Update HumanA.cpp

--- cpp_01/ex03/HumanA.cpp
+++ cpp_01/ex03/HumanA.cpp
@@ -12,11 +12,23 @@
 
 #include "HumanA.hpp"
 
+/**
+ * @brief Displays an attack message with the weapon used by HumanA.
+ *
+ * Uses the name of the human and the type of the weapon to display
+ * an attack message.
+ */
 void HumanA:: attack( void )
 {
 	std::cout << this->name << " attacks with their " << this->weapon.getType() << std::endl;
 }
 
+/**
+ * @brief Constructs a HumanA with a name and a reference to a weapon.
+ *
+ * @param name The name of the HumanA.
+ * @param weapon A reference to a Weapon object used by HumanA.
+ */
 HumanA::HumanA( std::string name, Weapon& weapon ) : weapon(weapon)
 {
 	this->name = name;
@@ -24,6 +36,11 @@ HumanA::HumanA( std::string name, Weapon& weapon ) : weapon(weapon)
 	std::cout << "HumanA Constructor" << std::endl;
 }
 
+/**
+ * @brief Destructor for HumanA.
+ *
+ * Prints a message indicating destruction of the HumanA object.
+ */
 HumanA::~HumanA( void )
 {
 	std::cout << "HumanA Contructor" << std::endl;
