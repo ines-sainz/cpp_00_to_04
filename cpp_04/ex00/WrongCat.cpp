8b81cff6ea402cfbb8106544ae59f527c1f4a64b
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 15:21:46 +0200

    Update WrongCat.cpp

--- cpp_04/ex00/WrongCat.cpp
+++ cpp_04/ex00/WrongCat.cpp
@@ -12,16 +12,34 @@
 
 #include "WrongCat.hpp"
 
+/**
+ * @brief Makes a wrong cat sound.
+ * 
+ * Outputs a non-standard "miau" sound to the console.
+ */
 void	WrongCat::makeSound( void ) const
 {
 	std::cout << "Wrong Miau" << std::endl;
 }
 
+/**
+ * @brief Gets the type of the WrongCat.
+ * 
+ * @return std::string The type as a string.
+ */
 std::string	WrongCat::getType( void ) const
 {
 	return (this->type);
 }
 
+/**
+ * @brief Copy assignment operator.
+ * 
+ * Assigns the type from another WrongCat.
+ * 
+ * @param before The WrongCat to copy from.
+ * @return WrongCat& Reference to the assigned object.
+ */
 WrongCat& WrongCat::operator=( const WrongCat& before )
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -29,18 +47,35 @@ WrongCat& WrongCat::operator=( const WrongCat& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Initializes a new WrongCat as a copy of another.
+ * 
+ * @param before The WrongCat to copy.
+ */
 WrongCat::WrongCat( const WrongCat& before )
 {
 	std::cout << "Copy Constructor Called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor.
+ * 
+ * Initializes a WrongCat with default type "WrongCat".
+ */
 WrongCat::WrongCat( void )
 {
 	std::cout << "Default WrongCat Constructor" << std::endl;
 	this->type = "WrongCat";
 }
 
+/**
+ * @brief Destructor.
+ * 
+ * Called when the WrongCat object is destroyed.
+ */
 WrongCat::~WrongCat()
 {
 	std::cout << "Default WrongCat Destructor" << std::endl;
