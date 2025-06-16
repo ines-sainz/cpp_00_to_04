8ec7543d84d7d53638e445872b2fca4a69287a6b
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 15:12:51 +0200

    Update Dog.cpp

--- cpp_04/ex00/Dog.cpp
+++ cpp_04/ex00/Dog.cpp
@@ -12,16 +12,34 @@
 
 #include "Dog.hpp"
 
+/**
+ * @brief Makes a dog sound.
+ * 
+ * Overrides the base Animal sound with a dog-specific sound.
+ */
 void	Dog::makeSound( void) const
 {
 	std::cout << "Guau" << std::endl;
 }
 
+/**
+ * @brief Gets the type of the Dog.
+ * 
+ * @return std::string The type of the Dog.
+ */
 std::string	Dog::getType( void ) const
 {
 	return (this->type);
 }
 
+/**
+ * @brief Copy assignment operator.
+ * 
+ * Copies the internal state from another Dog object.
+ * 
+ * @param before The Dog to copy from.
+ * @return Dog& Reference to the assigned object.
+ */
 Dog& Dog::operator=( const Dog& before )
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -29,18 +47,35 @@ Dog& Dog::operator=( const Dog& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Creates a new Dog as a copy of another.
+ * 
+ * @param before The Dog to copy.
+ */
 Dog::Dog( const Dog& before )
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor.
+ * 
+ * Initializes the Dog with default values.
+ */
 Dog::Dog( void )
 {
 	std::cout << "Default Dog Constructor" << std::endl;
 	this->type = "Dog";
 }
 
+/**
+ * @brief Destructor.
+ * 
+ * Called when the Dog object is destroyed.
+ */
 Dog::~Dog()
 {
 	std::cout << "Default Dog Destructor" << std::endl;
