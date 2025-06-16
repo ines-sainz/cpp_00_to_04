d754cbdfb1d3703e6601f9855cf0b716cf54b622
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 16:14:48 +0200

    Update Cure.cpp

--- cpp_04/ex03/Cure.cpp
+++ cpp_04/ex03/Cure.cpp
@@ -12,17 +12,37 @@
 
 #include "Cure.hpp"
 
+/**
+ * @brief Uses the Cure Materia on the target character.
+ * 
+ * Prints a message indicating that the target's wounds are healed.
+ * 
+ * @param target The character being healed.
+ */
 void Cure::use( ICharacter& target)
 {
 	std::cout << "* heals " << target.getName() << "’s wounds *" << std::endl;
 }
 
+/**
+ * @brief Creates a new copy of the Cure Materia.
+ * 
+ * @return AMateria* A pointer to a new Cure instance.
+ */
 AMateria* Cure::clone() const
 {
     AMateria *newMateria = new Cure();
     return (newMateria);
 }
 
+/**
+ * @brief Assignment operator for Cure.
+ * 
+ * Copies the type from another Cure instance.
+ * 
+ * @param before The Cure object to copy from.
+ * @return Cure& Reference to this Cure instance.
+ */
 Cure& Cure::operator=( const Cure& before )
 {
     std::cout << "Copy Cure Assigment operator called" << std::endl;
@@ -30,18 +50,35 @@ Cure& Cure::operator=( const Cure& before )
     return (*this);
 }
 
+/**
+ * @brief Copy constructor for Cure.
+ * 
+ * Initializes this Cure with the values of another Cure instance.
+ * 
+ * @param before The Cure object to copy.
+ */
 Cure::Cure( const Cure& before)
 {
     std::cout << "Copy Cure constructor called" << std::endl;
     *this = before;
 }
 
+/**
+ * @brief Default constructor for Cure.
+ * 
+ * Sets the type to "cure" and prints a construction message.
+ */
 Cure::Cure( void )
 {
     std::cout << "Default Cure Consructor called" << std::endl;
     this->type = "cure";
 }
 
+/**
+ * @brief Destructor for Cure.
+ * 
+ * Prints a message indicating Cure destruction.
+ */
 Cure::~Cure()
 {
     std::cout << "Default Cure Destructor called" << std::endl;
