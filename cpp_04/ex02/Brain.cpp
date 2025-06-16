3526433a6a803b883089f4f9d39e33a2adfe4ff2
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 15:50:26 +0200

    Update Brain.cpp

--- cpp_04/ex02/Brain.cpp
+++ cpp_04/ex02/Brain.cpp
@@ -12,17 +12,36 @@
 
 #include "Brain.hpp"
 
+/**
+ * @brief Sets the same idea string to all elements in the ideas array.
+ * 
+ * @param idea The idea string to be assigned to each slot.
+ */
 void	Brain::setIdeas( std::string idea )
 {
 	for (size_t i = 0; i < 100; i++)
 		this->ideas[i] = idea;
 }
+
+/**
+ * @brief Prints all ideas stored in the Brain.
+ * 
+ * Outputs each idea in the array to the standard output.
+ */
 void	Brain::printIdeas( void )
 {
 	for (size_t i = 0; i < 100; i++)
 		std::cout << this->ideas[i] << std::endl;
 }
 
+/**
+ * @brief Copy assignment operator for Brain.
+ * 
+ * Copies each element from another Brain's ideas array.
+ * 
+ * @param before The Brain instance to copy from.
+ * @return Brain& Reference to the assigned Brain object.
+ */
 Brain& Brain::operator=( const Brain& before )
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -31,12 +50,24 @@ Brain& Brain::operator=( const Brain& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor for Brain.
+ * 
+ * Initializes a new Brain as a copy of an existing one.
+ * 
+ * @param before The Brain instance to copy.
+ */
 Brain::Brain( const Brain& before )
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor for Brain.
+ * 
+ * Initializes the ideas array with a default message.
+ */
 Brain::Brain( void )
 {
 	std::cout << "Default Brain Constructor Called" << std::endl;
@@ -45,6 +76,11 @@ Brain::Brain( void )
 	
 }
 
+/**
+ * @brief Destructor for Brain.
+ * 
+ * Outputs a message when a Brain instance is destroyed.
+ */
 Brain::~Brain()
 {
 	std::cout << "Default Brain Destructor Called" << std::endl;
