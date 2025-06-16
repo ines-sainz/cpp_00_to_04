69bef31738ad7cd3164efdd22adbe782526227fc
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 15:24:27 +0200

    Update Animal.cpp

--- cpp_04/ex01/Animal.cpp
+++ cpp_04/ex01/Animal.cpp
@@ -12,16 +12,34 @@
 
 #include "Animal.hpp"
 
+/**
+ * @brief Makes a generic animal sound.
+ * 
+ * This method is meant to be overridden in derived classes.
+ */
 void	Animal::makeSound( void ) const
 {
 	std::cout << "Noises" << std::endl;
 }
 
+/**
+ * @brief Gets the type of the animal.
+ * 
+ * @return std::string The type of the animal.
+ */
 std::string	Animal::getType( void ) const
 {
 	return (this->type);
 }
 
+/**
+ * @brief Copy assignment operator.
+ * 
+ * Copies the internal state from another Animal object.
+ * 
+ * @param before The Animal to copy from.
+ * @return Animal& Reference to the assigned object.
+ */
 Animal& Animal::operator=( const Animal& before )
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -29,18 +47,35 @@ Animal& Animal::operator=( const Animal& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Creates a new Animal as a copy of another.
+ * 
+ * @param before The Animal to copy.
+ */
 Animal::Animal( const Animal& before )
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor.
+ * 
+ * Initializes the Animal with a default type.
+ */
 Animal::Animal( void )
 {
 	this->type = "Animal";
 	std::cout << "Default Animal Constructor" << std::endl;
 }
 
+/**
+ * @brief Destructor.
+ * 
+ * Called when the Animal object is destroyed.
+ */
 Animal::~Animal()
 {
 	std::cout << "Default Animal Destructor" << std::endl;
