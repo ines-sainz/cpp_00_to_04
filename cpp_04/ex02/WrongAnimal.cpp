977aa7098cd0cceb0a9c720c00fc86083e90061b
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 15:56:25 +0200

    Update WrongAnimal.cpp

--- cpp_04/ex02/WrongAnimal.cpp
+++ cpp_04/ex02/WrongAnimal.cpp
@@ -12,16 +12,34 @@
 
 #include "WrongAnimal.hpp"
 
+/**
+ * @brief Makes a generic wrong animal sound.
+ * 
+ * Outputs a default "wrong" sound to the console.
+ */
 void	WrongAnimal::makeSound( void ) const
 {
 	std::cout << "Wrong Noises" << std::endl;
 }
 
+/**
+ * @brief Gets the type of the WrongAnimal.
+ * 
+ * @return std::string The type as a string.
+ */
 std::string	WrongAnimal::getType( void ) const
 {
 	return (this->type);
 }
 
+/**
+ * @brief Copy assignment operator.
+ * 
+ * Assigns the type from another WrongAnimal.
+ * 
+ * @param before The WrongAnimal to copy from.
+ * @return WrongAnimal& Reference to the assigned object.
+ */
 WrongAnimal& WrongAnimal::operator=( const WrongAnimal& before )
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -29,20 +47,36 @@ WrongAnimal& WrongAnimal::operator=( const WrongAnimal& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Initializes a new WrongAnimal as a copy of another.
+ * 
+ * @param before The WrongAnimal to copy.
+ */
 WrongAnimal::WrongAnimal( const WrongAnimal& before )
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor.
+ * 
+ * Initializes a WrongAnimal with default type.
+ */
 WrongAnimal::WrongAnimal( void )
 {
 	this->type = "WrongAnimal";
 	std::cout << "Default WrongAnimal Constructor" << std::endl;
 }
 
+/**
+ * @brief Destructor.
+ * 
+ * Called when the WrongAnimal object is destroyed.
+ */
 WrongAnimal::~WrongAnimal()
 {
 	std::cout << "Default WrongAnimal Destructor" << std::endl;
 }
-
