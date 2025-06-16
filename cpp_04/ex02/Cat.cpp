24a7c59c5dae53334c89b30b028b06c37d7be595
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 15:52:01 +0200

    Update Cat.cpp

--- cpp_04/ex02/Cat.cpp
+++ cpp_04/ex02/Cat.cpp
@@ -12,25 +12,50 @@
 
 #include "Cat.hpp"
 
+/**
+ * @brief Sets all ideas in the Cat's Brain to the given idea.
+ * 
+ * @param idea The idea string to assign to all Brain slots.
+ */
 void	Cat::setIdeas( std::string idea )
 {
 	this->brain->setIdeas(idea);
 }
+
+/**
+ * @brief Prints all the ideas stored in the Cat's Brain.
+ */
 void	Cat::printIdeas( void )
 {
 	this->brain->printIdeas();
 }
 
+/**
+ * @brief Outputs the Cat's sound to the console.
+ */
 void	Cat::makeSound( void ) const
 {
 	std::cout << "Miau" << std::endl;
 }
 
+/**
+ * @brief Returns the type of the Cat.
+ * 
+ * @return std::string The type of the Cat.
+ */
 std::string	Cat::getType( void ) const
 {
 	return (this->type);
 }
 
+/**
+ * @brief Copy assignment operator for Cat.
+ * 
+ * Copies the type and creates a new copy of the Brain.
+ * 
+ * @param before The Cat instance to copy from.
+ * @return Cat& Reference to the assigned Cat object.
+ */
 Cat& Cat::operator=( const Cat& before )
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -39,12 +64,24 @@ Cat& Cat::operator=( const Cat& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor for Cat.
+ * 
+ * Initializes a new Cat as a copy of another.
+ * 
+ * @param before The Cat instance to copy.
+ */
 Cat::Cat( const Cat& before )
 {
 	std::cout << "Copy Constructor Called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor for Cat.
+ * 
+ * Initializes type to "Cat" and allocates a new Brain.
+ */
 Cat::Cat( void )
 {
 	std::cout << "Default Cat Constructor" << std::endl;
@@ -52,6 +89,11 @@ Cat::Cat( void )
 	this->brain = new Brain();
 }
 
+/**
+ * @brief Destructor for Cat.
+ * 
+ * Frees the dynamically allocated Brain.
+ */
 Cat::~Cat()
 {
 	delete this->brain;
