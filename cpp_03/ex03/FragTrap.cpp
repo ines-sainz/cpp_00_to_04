9f7cebe658efe5014cd2acb439f64a5b3f9fac52
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 14:30:03 +0200

    Update FragTrap.cpp

--- cpp_03/ex03/FragTrap.cpp
+++ cpp_03/ex03/FragTrap.cpp
@@ -12,11 +12,20 @@
 
 #include "FragTrap.hpp"
 
+/**
+ * @brief Prompts a high five from the FragTrap.
+ */
 void	FragTrap::highFivesGuys( void )
 {
 	std::cout << "FragTrap " << this->name << " ask for a high five" << std::endl;
 }
 
+/**
+ * @brief Copy assignment operator for FragTrap.
+ * 
+ * @param before The FragTrap object to copy from.
+ * @return FragTrap& Reference to the assigned object.
+ */
 FragTrap& FragTrap:: operator=(const FragTrap& before)
 {
 	std::cout << "Copy assigment operator called" << std::endl;
@@ -27,12 +36,22 @@ FragTrap& FragTrap:: operator=(const FragTrap& before)
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor for FragTrap.
+ * 
+ * @param before The FragTrap object to copy from.
+ */
 FragTrap::FragTrap(const FragTrap& before)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Default constructor for FragTrap.
+ * 
+ * Initializes default values for name, hitPoints, energy, and damage.
+ */
 FragTrap::FragTrap( void )
 {
 	std::cout << "Default FragTrap Constructor" << std::endl;
@@ -42,6 +61,11 @@ FragTrap::FragTrap( void )
 	this->AttackDamage = 30;
 }
 
+/**
+ * @brief Parameterized constructor for FragTrap.
+ * 
+ * @param name The name to assign to the FragTrap.
+ */
 FragTrap::FragTrap( std::string name )
 {
 	this->name = name;
@@ -50,8 +74,13 @@ FragTrap::FragTrap( std::string name )
 	this->EnergyPoints = 100;
 	this->AttackDamage = 30;
 }
+
+/**
+ * @brief Destructor for FragTrap.
+ * 
+ * Prints a message when the object is destroyed.
+ */
 FragTrap::~FragTrap()
 {
 	std::cout << "Default FragTrap Destructor" << std::endl;
 }
-
